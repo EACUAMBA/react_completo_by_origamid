0cbeb9efc9d3b8006a073001abc64d351948757a
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-06-18 01:34:06 +0200

    Para gerir a seleção de checkboxes.

---
+++ 04-formularios/09-componentes-checkbox/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Input from "./Input";
+import Select from "./Select";
+import Radio from "./Radio";
+import Checkbox from "./Checkbox";
+
+const App = () => {
+
+    const [nome, setNome] = React.useState('');
+    const [email, setEmail] = React.useState('');
+
+    const [sexo, setSexo] = React.useState('');
+    const sexos = ['Masculino', 'Feminino'];
+
+    const [frutas, setFrutas] = React.useState([]);
+    const [termos, setTermos] = React.useState([]);
+
+    return (
+        <>
+            <form>
+                <Checkbox checkboxes={['Mamão', 'Ananaz', 'Melancia', 'Pera']} setValue={setFrutas} value={frutas}/>
+                <Checkbox checkboxes={['Li e aceito os termos']} setValue={setTermos} value={termos}/>
+                <Input id={'nome'} label={'Nome'} name={'nome'} value={nome} setValue={setNome}/>
+                <Input id={'email'} label={'Email'} name={'email'} value={email} setValue={setEmail} required/>
+                <Select value={sexo} setValue={setSexo} options={sexos} />
+                <Radio value={sexo} setValue={setSexo} radios={sexos}/>
+                <br/>
+                <button type={"submit"}>Enviar</button>
+            </form>
+        </>
+    );
+};
+
+
+export default App;
