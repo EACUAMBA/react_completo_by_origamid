920dd3d2e019d320a6c48234c5df4244acb8ed16
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-07-03 23:47:13 +0200

    Aprendi a utilizar a biblioteca react router dom, para navegar entre páginas.

---
+++ 06-react-router/01-router/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(){
+    return (
+        <div style={{width: '100%', height: '64px', background: '#123456'}}>
+            <h1>HEADER</h1>
+        </div>
+    );
+}
+
+export default Header;
