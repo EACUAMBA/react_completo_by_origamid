c41976d9b07d06685cd7c5c8f542c6db2262b0ec
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-07-24 01:52:49 +0200

    Implementei o react.context para enviar o estado da aplicação entre diferentes objectos.

--- 08-projecto-final/dogs/src/pages/Login/LoginForm/index.js
+++ 08-projecto-final/dogs/src/pages/Login/LoginForm/index.js
@@ -5,10 +5,12 @@ import Input from "../../../components/forms/Input";
 import Button from "../../../components/forms/Button";
 import useForm from "../../../hooks/useForm";
 import {TOKEN_POST, USER_GET} from "../../../api";
+import {UserContext} from "../../../UserContext";
 
 const LoginForm = () => {
     const username = useForm();
     const password = useForm('password');
+    const {userLogin, getUser} = React.useContext(UserContext);
 
     React.useEffect(()=>{
         const token = window.localStorage.getItem('token');
@@ -17,25 +19,10 @@ const LoginForm = () => {
         }
     }, [])
 
-    async function getUser(token) {
-        const {url, options} = USER_GET(token);
-        const response = await fetch(url, options);
-        const json = await response.json();
-        console.log(json);
-    }
-
     async function handleSubmit(event) {
         event.preventDefault();
 
-        if(username.validate() && password.validate()){
-           const {url, options} = TOKEN_POST({username: username.value, password: password.value})
-
-           const response = await fetch(url, options);
-           const json = await response.json();
-
-           window.localStorage.setItem('token', json.token);
-           getUser(json.token);
-        }
+        userLogin(username.value, password.value)
     }
 
     return (
