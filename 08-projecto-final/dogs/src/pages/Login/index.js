c88ebe8b5aa4c8ce97f29cff11ce733587ecc0b6
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-07-26 21:03:06 +0200

    Adicionei background na tela de login, nas telas.

--- 08-projecto-final/dogs/src/pages/Login/index.js
+++ 08-projecto-final/dogs/src/pages/Login/index.js
@@ -16,14 +16,16 @@ const Login = () => {
     }
 
     return (
-        <div className={styles.login}>
-            <Routes>
-                <Route path={'/'} element={<LoginForm/>}/>
-                <Route path={'/criar'} element={<LoginCreate/>}/>
-                <Route path={'/perdeu'} element={<LoginPasswordLost/>}/>
-                <Route path={'/resetar'} element={<LoginPasswordReset/>}/>
-            </Routes>
-        </div>
+        <section className={styles.login}>
+            <div className={styles.forms}>
+                <Routes>
+                    <Route path={'/'} element={<LoginForm/>}/>
+                    <Route path={'/criar'} element={<LoginCreate/>}/>
+                    <Route path={'/perdeu'} element={<LoginPasswordLost/>}/>
+                    <Route path={'/resetar'} element={<LoginPasswordReset/>}/>
+                </Routes>
+            </div>
+        </section>
     );
 };
 
