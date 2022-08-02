9d3ac51ebcc65b9b603d048a9d97c7c1b4997ee9
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-08-02 19:55:14 +0200

    #feature: Componente das fotos no feed já está concluido.

---
+++ 08-projecto-final/dogs/src/pages/Feed/FeedPhotos/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './index.module.css'
+import FeedPhotosItem from "../../../components/FeedPhotosItem";
+import useFetch from "../../../hooks/useFetch";
+import {PHOTOS_GET} from "../../../api";
+import Error from "../../../components/helper/Error";
+import Loading from "../../../components/helper/Loading";
+
+const FeedPhotos= () => {
+    const {data, loading, error, request} = useFetch();
+
+    React.useEffect(
+        () => {
+            async function fetchPhotos() {
+                const {url, options} = PHOTOS_GET({page: 1, total: 6, user: 0});
+                const requestResult = await request(url, options);
+                console.log(requestResult);
+            }
+            fetchPhotos();
+        }
+        , [request]
+    )
+    if(error) return <Error error={error}/>;
+    if(loading) return <Loading/>
+    if(!data) return null;
+    return (
+        <ul className={`animeLeft ${styles.feed}`}>
+            {
+                data.map(photo => {
+                    return <li className={styles.photo} key={photo.id}><FeedPhotosItem  photo={photo}/></li>
+                })
+            }
+        </ul>
+    );
+};
+
+export default FeedPhotos;
