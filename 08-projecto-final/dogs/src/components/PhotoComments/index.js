be2b074e6b2e3ba5ae163d41fcae75d7225d1b57
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-08-02 21:37:29 +0200

    #feature: adicionando o componente de apresentação de comentarios.

---
+++ 08-projecto-final/dogs/src/components/PhotoComments/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './index.module.css';
+
+const PhotoComments = () => {
+
+    return (
+        <h1>Comentarios</h1>
+    );
+};
+
+export default PhotoComments;
