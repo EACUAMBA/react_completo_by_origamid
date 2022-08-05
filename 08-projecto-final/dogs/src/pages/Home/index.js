a59f4eca9821898052b31124208beba02e14c631
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-08-05 22:18:17 +0200

    @improvement: agora temos um loading digno.

--- 08-projecto-final/dogs/src/pages/Home/index.js
+++ 08-projecto-final/dogs/src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './index.module.css';
 import Feed from "../Feed";
+import Loading from "../../components/helper/Loading";
 
 const Home = () => {
     return (
         <section className={`container mainContainer ${styles.home}`}>
             <Feed/>
+
         </section>
     );
 };
