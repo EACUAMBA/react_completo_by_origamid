0e860f12a72d9c8bf11f804836a10f83e9dfca2f
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-08-05 21:23:48 +0200

    @feature: temos o skeleton antes de carregar uma imagem.

--- 08-projecto-final/dogs/src/components/FeedPhotosItem/index.js
+++ 08-projecto-final/dogs/src/components/FeedPhotosItem/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './index.module.css'
+import Image from "../helper/Image";
 
 const FeedPhotosItem = ({photo}) => {
     return (
         <>
-            <img src={photo.src} alt={photo.title}/>
+            <Image alt={photo.title} src={photo.src}/>
             <span className={styles.visualizacao}>{photo.acessos}</span>
         </>
 
---
+++ 08-projecto-final/dogs/src/components/helper/Image/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './index.module.css';
+
+const Image = ({alt, ...props}) => {
+    const [skeleton, setSkeleton] = React.useState(true);
+    function handleLoad(event) {
+        event.target.style.opacity = 1;
+        setSkeleton(false)
+        console.log(skeleton)
+    }
+    return (
+        <div className={styles.wrapper}>
+            {skeleton ? <div className={styles.skeleton}/> : null}
+            <img onLoad={handleLoad} className={styles.img} alt={alt} {...props}/>
+        </div>
+    );
+};
+
+export default Image;
