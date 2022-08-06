00ca7b82daa455a917a9106018641449f01b5fe4
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-08-06 16:23:47 +0200

    @feature: o site ja tem footer.

--- 08-projecto-final/dogs/src/App.js
+++ 08-projecto-final/dogs/src/App.js
@@ -17,16 +17,20 @@ function App() {
         <>
             <BrowserRouter>
                 <UserStorage>
-                    <Header/>
-                    <Routes>
-                        <Route path={'/'} element={<Home/>}/>
-                        <Route path={'login/*'} element={<Login/>}/>
-                        <Route path={'conta/*'} element={<ProtectedRoute><User/></ProtectedRoute>}/>
-                        <Route path={'foto/:id'} element={<Photo/>}/>
-                        <Route path={'perfil/:user'} element={<UserProfile/>}/>
-                        <Route path={'*'} element={<NotFound404/>}/>
-                    </Routes>
-                    <Footer/>
+                    <div className={`app`}>
+                        <Header/>
+                        <main className={`appMain`}>
+                            <Routes>
+                                <Route path={'/'} element={<Home/>}/>
+                                <Route path={'login/*'} element={<Login/>}/>
+                                <Route path={'conta/*'} element={<ProtectedRoute><User/></ProtectedRoute>}/>
+                                <Route path={'foto/:id'} element={<Photo/>}/>
+                                <Route path={'perfil/:user'} element={<UserProfile/>}/>
+                                <Route path={'*'} element={<NotFound404/>}/>
+                            </Routes>
+                        </main>
+                        <Footer/>
+                    </div>
                 </UserStorage>
             </BrowserRouter>
         </>
--- 08-projecto-final/dogs/src/components/Footer/index.js
+++ 08-projecto-final/dogs/src/components/Footer/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './index.module.css';
+import {ReactComponent as DogsFooter} from "./../../assets/dogs-footer.svg";
 
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-Footer
+            <DogsFooter/>
+            <p>Dogs. Alguns Direitos Reservados</p>
         </footer>
     );
 };
