4bb8451799aaf0cfac5641d4594dd7928a4b9220
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-08-06 21:20:28 +0200

    @feature: o site ja tem gráfico.

--- 08-projecto-final/dogs/src/pages/User/UserStats/index.js
+++ 08-projecto-final/dogs/src/pages/User/UserStats/index.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import useFetch from "../../../hooks/useFetch";
+import {STATS_GET} from "../../../api";
+import Loading from "../../../components/helper/Loading";
+import Error from "../../../components/helper/Error";
+import {ReactComponent} from "*.svg";
+const UserStatsGraphs = React.lazy(()=> import("../../../components/UserStatsGraphs")) ;
 
 const UserStats = () => {
-    return (
-        <div>
+    const {request, error, loading, data} = useFetch();
 
-        </div>
-    );
+    React.useEffect(() => {
+        async function fetchStats() {
+            const {url, options} = STATS_GET();
+            const {response} = await request(url, options);
+            if (response && response.ok) {
+                console.log('Executou')
+                console.log(data)
+            }
+        }
+
+        fetchStats()
+    }, [request])
+
+    if (loading) return <Loading/>
+    if (error) return <Error error={error}/>
+    if (data)
+        return (
+            <div>
+               <React.Suspense fallback={<p>Conecte-se a internet faz favor.</p>}>
+                   <UserStatsGraphs data={data} />
+               </React.Suspense>
+            </div>
+        );
 };
 
 export default UserStats;
